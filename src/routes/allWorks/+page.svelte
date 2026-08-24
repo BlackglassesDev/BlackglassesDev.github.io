9215a1f517042fcaa22be64a17eab0f29e7ae080
BlackglassesDev <ochanodom@gmail.com>
2026-08-25 02:17:08 +0700

    feat: add coming soon page for portfolio works section

---
+++ src/routes/allWorks/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import logo from '../../lib/assets/logo.png'
+</script>
+
+<div
+	class="container-max mx-auto flex min-h-screen w-full flex-col items-center justify-center gap-4 text-center text-2xl text-on-surface-variant"
+>
+	<img src={logo} alt="Blackglasses-Logo" class="h-60 w-60 object-contain" />
+	Coming Soon....
+</div>
